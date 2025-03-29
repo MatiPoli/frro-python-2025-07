a9b3586db7544eb1ef97cce9b6b7a327139b5590
Lucio Gilardoni <gilardoni.lucio@gmail.com>
2025-03-29 11:54:57 -0300

    Update ejercicio_02.py

--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -7,6 +7,12 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
     Restricción: Utilizar UNICAMENTE tres IFs y comparaciones encadenadas.
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons
     """
+    if b < a > c:
+        return a
+    if a < b > c:
+        return b
+    if a < c > b:
+        return c
     pass # Completar
 
 
@@ -16,7 +22,6 @@ assert maximo_encadenado(4, 9, 18) == 18
 assert maximo_encadenado(24, 9, 18) == 24
 # NO MODIFICAR - FIN
 
-
 ###############################################################################
 
 
@@ -24,6 +29,7 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
+    return max(a,b,c,d)
     pass # Completar
 
 
@@ -34,7 +40,6 @@ assert maximo_cuadruple(24, 9, 18, 20) == 24
 assert maximo_cuadruple(24, 9, 18, 30) == 30
 # NO MODIFICAR - FIN
 
-
 ###############################################################################
 
 
@@ -42,6 +47,7 @@ def maximo_arbitrario(*args) -> float:
     """Re-escribir para que tome una cantidad arbitraria de parámetros.
     Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
     """
+    return max(args)
     pass # Completar
 
 
@@ -51,7 +57,6 @@ assert maximo_arbitrario(4, 9, 18, 6) == 18
 assert maximo_arbitrario(24, 9, 18, 20) == 24
 assert maximo_arbitrario(24, 9, 18, 30) == 30
 # NO MODIFICAR - FIN
-
 
 ###############################################################################
 
