fc075bf0d3f2e6ffc79c5555b2853667bcba03b1
MatiPoli <matiastmarquez@gmail.com>
2025-09-13 13:13:31 -0300

    Arreglos varios y agregado de boton

--- TPI/ytFollowers/urls.py
+++ TPI/ytFollowers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ytfollowers_home'),
+    path('followers/', views.home, name='ytfollowers_home'),
 ]
